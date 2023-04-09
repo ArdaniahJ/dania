31ecfb6d9dbc7ee2cd832cb4bf2ea7a3fbfdc370
apple <apple@shims-iMac.local>
2023-04-10 04:46:24 +0800

    Updates

---
+++ precache-manifest.1665c604ae54f67668fa046ba6cc57a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67a87378ba4ce0a3702a74cbfaa9e17",
+    "url": "/dania/index.html"
+  },
+  {
+    "revision": "9756b53c684d6e9eaa55",
+    "url": "/dania/static/css/2.3f074615.chunk.css"
+  },
+  {
+    "revision": "c335ac2c37e441e5a9f7",
+    "url": "/dania/static/css/main.e4b21b1f.chunk.css"
+  },
+  {
+    "revision": "9756b53c684d6e9eaa55",
+    "url": "/dania/static/js/2.e7c76e23.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/dania/static/js/2.e7c76e23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c335ac2c37e441e5a9f7",
+    "url": "/dania/static/js/main.b0502c97.chunk.js"
+  },
+  {
+    "revision": "3fa16a39cb747c97f6db",
+    "url": "/dania/static/js/runtime-main.cafcfc3f.js"
+  }
+]);
